4c3672668d39936197251af4dc5742f5cd4190cf
nkxzq <agregonace@gmail.com>
2023-12-15 21:08:03 +0300

    сделал красивые границы + ходящий влево вправо человечек

--- main.py
+++ main.py
@@ -12,37 +12,62 @@ pygame.display.set_caption('Russian Mario')
 tile_size = 50
 
 
-#zagruzgaem kartinki
-#sun_img = pygame.image.load('img/sun.png')
 background = pygame.image.load('img/sky.png')
 
-#def draw_grid():
-#    for line in range(0, 6):
-#        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size, screen_width))
-#        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
+class Player():
+    def __init__(self, x, y):
+        image = pygame.image.load('extra/Alien sprites/alienPink.png')
+        self.image = pygame.transform.scale(image, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+
+        new_x = 0
+        new_y = 0
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            new_x -= 5
+        if key[pygame.K_RIGHT]:
+            new_x += 5
+
+
+        self.rect.x += new_x
+        self.rect.y += new_y
+
+        screen.blit(self.image, self.rect)
 
 
 class World():
     def __init__(self, data):
         self.tile_list = []
 
-        snow = pygame.image.load('ice/Tiles/tundra.png')
-        another = pygame.image.load('candy/Tiles/cake.png')
+        snow = pygame.image.load('ice/Tiles/tundraMid.png')
+        candy = pygame.image.load('candy/Tiles/cake.png')
+        snow_center = pygame.image.load('ice/Tiles/tundraCenter.png')
 
         row_count = 0
         for row in data:
             count = 0
             for tile in row:
                 if tile == 1:
-                    img = pygame.transform.scale(another, (tile_size, tile_size))
+                    img = pygame.transform.scale(snow_center, (tile_size, tile_size))
                     rect = img.get_rect()
                     rect.x = count * tile_size
                     rect.y = row_count * tile_size
                     tile = (img, rect)
                     self.tile_list.append(tile)
                 if tile == 2:
-                    img = pygame.transform.scale(another, (tile_size, tile_size))
+                    img = pygame.transform.scale(candy, (tile_size, tile_size))
+                    rect = img.get_rect()
+                    rect.x = count * tile_size
+                    rect.y = row_count * tile_size
+                    tile = (img, rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    img = pygame.transform.scale(snow, (tile_size, tile_size))
                     rect = img.get_rect()
                     rect.x = count * tile_size
                     rect.y = row_count * tile_size
@@ -76,19 +101,21 @@ world = [
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+[1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1]
 ]
 
+
+player = Player(100, screen_height - 130)
 world = World(world)
 
 run = True
 while run:
 
     screen.blit(background, (0, 0))
-    #screen.blit(sun_img, (100, 100))
 
     world.draw()
-#    draw_grid()
+    player.update()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
