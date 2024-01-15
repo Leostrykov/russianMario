00aa9dcb8f4f70410037639b8e7c8e194ef6a521
Leonid Strukov <leostrukov@ya.ru>
2024-01-15 16:45:53 +0300

    Добавили монеты, настроили начальный экран, звуковые эффекты, исправили gameover. Коммит nkxzq.

--- Tiles.py
+++ Tiles.py
@@ -5,9 +5,9 @@ tiles = {
     'ground_start': pygame.image.load('img/Tiles/tile_0001.png'), #1
     'ground_center': pygame.image.load('img/Tiles/tile_0002.png'), #2
     'ground_end': pygame.image.load('img/Tiles/tile_0003.png'),   #3
-    'pass': (''), #4-блок для врага
     'ground_underground': pygame.image.load('img/Tiles/tile_0006.png'), #5
     'water': pygame.image.load('img/Tiles/tile_0053.png'), #6
+    'coin': pygame.image.load('img/Tiles/tile_0151.png'), #7
 }
 # получение списка ключей словаря tiles
 tiles_name = list(tiles.keys())
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pygame
 from pygame.locals import *
 from Tiles import tiles, tiles_name
+from pygame import mixer
+import sys
 
 
 game_over = 0
 main_menu = True
 action = False
+mixer.init()
+pygame.mixer.pre_init(44100, -16, 2, 512)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, *group):
         super().__init__(*group)
         self.main(x, y)
 
-
     def main(self, x, y):
-
         global main_menu
 
         self.images_walk = []
@@ -55,6 +57,7 @@ class Player(pygame.sprite.Sprite):
             new_y = 0
             key = pygame.key.get_pressed()
             if key[pygame.K_UP] and self.jumped is False:
+                jump_sound.play()
                 if self.jumped_count != 2:
                     self.vel_y = -15
                     self.jumped = True
@@ -100,9 +103,11 @@ class Player(pygame.sprite.Sprite):
 
             if pygame.sprite.spritecollide(self, enemy_group, False):
                 game_over = -1
+                game_over_sound.play()
                 print('enemy')
             if pygame.sprite.spritecollide(self, lava_group, False):
                 game_over = -1
+                game_over_sound.play()
                 print('lava')
 
             self.rect.x += new_x
@@ -128,7 +133,7 @@ class World:
         for row_count, row in enumerate(data):
             col_count = 0
             for count, tile in enumerate(row):
-                if tile - 1 <= len(tiles_name) and tile != 0 and tile != 4 and tile != 6:
+                if tile - 1 <= len(tiles_name) and tile != 0 and tile != 4 and tile != 6 and tile != 7:
                     img = pygame.transform.scale(tiles[tiles_name[tile - 1]], (tile_size, tile_size))
                     rect = img.get_rect()
                     rect.x = count * tile_size
@@ -141,7 +146,10 @@ class World:
                 if tile == 6:
                     lava = Lava(count * tile_size, row_count * tile_size)
                     lava_group.add(lava)
-            col_count += 1
+                if tile == 7:
+                    coin = Coin(count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+                    coin_group.add(coin)
+        col_count += 1
         row_count += 1
 
     def draw(self):
@@ -179,6 +187,14 @@ class Lava(pygame.sprite.Sprite):
         self.rect.y = y
 
 
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/Tiles/tile_0151.png')
+        self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
 class Button():
     def __init__(self, x, y, image):
         self.image = image
@@ -204,7 +220,9 @@ class Button():
         return action
 
 
-
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 
 
@@ -222,23 +240,37 @@ world = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 4, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 3, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 6, 6, 2, 2, 2, 2, 2, 3]
 ]
 
 screen_width = 600
 screen_height = 600
+tile_size = 30
 enemy_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
+
+score_coin = Coin(tile_size // 2, tile_size // 2)
+coin_group.add(score_coin)
+
 start_btn = pygame.image.load('img/start_btn.png')
 restart_img = pygame.image.load('img/restart_btn.png')
 start_img = pygame.image.load('img/start_btn.png')
 exit_img = pygame.image.load('img/exit_btn.png')
+
+coin_sound = pygame.mixer.Sound('sound/coin.wav')
+coin_sound.set_volume(0.5)
+jump_sound = pygame.mixer.Sound('sound/jump.wav')
+jump_sound.set_volume(0.5)
+game_over_sound = pygame.mixer.Sound('sound/game_over.wav')
+game_over_sound.set_volume(0.5)
+
 start_img = pygame.transform.scale(start_img, (140, 60))
 exit_img = pygame.transform.scale(exit_img, (140, 60))
 
@@ -247,10 +279,6 @@ start_button = Button(screen_width // 2 - 225, screen_height // 2 - 50, start_im
 exit_button = Button(screen_width // 2 + 40, screen_height // 2 - 50, exit_img)
 
 
-circle_radius = 0
-circle_color = (255, 255, 0)
-draw_circle = False
-mouse_pos = (0, 0)
 
 if __name__ == '__main__':
     pygame.init()
@@ -263,9 +291,18 @@ if __name__ == '__main__':
 
     # размер одного блока (клеточки)
     tile_size = 30
+    score = 0
+    #font = pygame.font.SysFont('counter', 70)
+    font_score = pygame.font.SysFont('counter', 30)
+    font_start = pygame.font.SysFont('start', 70)
+    font_dev = pygame.font.SysFont('dev', 30)
     all_sprites = pygame.sprite.Group()
     player = Player(100, screen_height - 130, all_sprites)
     world = World(world)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    blue = (0, 0, 255)
+    red = (255, 0, 0)
 
     # enemy_group = pygame.sprite.Group()
     run = True
@@ -275,23 +312,38 @@ if __name__ == '__main__':
         if main_menu is True:
             if action is True:
                 main_menu = False
-            start_button.draw()
-            exit_button.draw()
+                action = False
+            draw_text('RUSSIAN MARIO', font_start, white, screen_width // 2 - 220, screen_height // 2 - 200)
+            draw_text('by:', font_dev, white, screen_width - 180, screen_height // 2 + 180)
+            draw_text('maxim antoniak', font_dev, white, screen_width - 180, screen_height // 2 + 200)
+            draw_text('leo strukov', font_dev, blue, screen_width - 180, screen_height // 2 + 220)
+            draw_text('egor barannikov', font_dev, red, screen_width - 180, screen_height // 2 + 240)
+            if exit_button.draw():
+                sys.exit()
+            if start_button.draw():
+                main_menu = False
         else:
-            screen.fill(pygame.Color((0, 246, 245)))
             clock.tick(fraps)
             all_sprites.draw(screen)
             all_sprites.update()
             world.draw()
             enemy_group.update()
+            if pygame.sprite.spritecollide(player, coin_group, True):
+                score += 1
+                coin_sound.play()
+                #print(score)
+            draw_text('X ' + str(score), font_score, white, tile_size, 8)
             enemy_group.draw(screen)
             lava_group.draw(screen)
+            coin_group.draw(screen)
             player.update()
 
             if game_over == -1:
-                if restart_button.draw():
-                    Player(100, screen_height - 130, all_sprites)
+                if restart_button.draw() and restart_button.clicked:
+                    Player.main(player, 100, screen_height - 130)
                     game_over = 0
+                    score = 0
+                    print(score)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
